95dbc3222b17e6ee8c0529ee940899c9052877f4
hughsim2 <49271645+hughsim2@users.noreply.github.com>
2020-06-20 05:18:49 +0000

    Add files via upload

---
+++ Disable WSH Locally.ps1
@@ -0,0 +1,2 @@
+﻿reg add "HKCU\Software\Microsoft\Windows Script Host\Settings" /v "Enabled" /t REG_DWORD /d "0" /f
+reg add "HKLM\Software\Microsoft\Windows Script Host\Settings" /v "Enabled" /t REG_DWORD /d "0" /f
---
+++ Disable WSH Remotely.ps1
@@ -0,0 +1,6 @@
+﻿
+
+Invoke-Command -ComputerName client -ScriptBlock {
+    Set-Itemproperty -path 'HKCU:\Software\Microsoft\Windows Script Host\Settings' -Name 'Enabled' -value '0'
+    Set-Itemproperty -path 'HKLM:\Software\Microsoft\Windows Script Host\Settings' -Name "Enabled" -value '0'
+}
